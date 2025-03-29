eec6f4ee5740d1f9289a31a7270009049ff3bbd7
FredVinet <fvinet9@gmail.com>
2025-03-29 23:25:16 +0100

    Window setup + Ball & Pad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Casse-Briques")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        draw_rectangle(screen_width() / 2.0 - 50.0, screen_height() - 30.0, 100.0, 10.0, WHITE);
+        draw_circle(screen_width() / 2.0, screen_height() / 2.0, 10.0, RED);
+
+        next_frame().await;
+    }
 }
